771d33faa4193530e6ddc1ded7373a94ae858411
Ya-Tzu Wang <yacat24@gmail.com>
2020-09-24 12:21:22 -0400

    add simple nav bar and shopping cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
 import './App.css';
+import Cart from "./components/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4" >
+            Store
+          </Typography>
+          <Cart/>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }

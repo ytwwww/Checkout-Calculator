cd815e2b673fc659586558696f5f165254aa6b21
Ya-Tzu Wang <52704719+ytwwww@users.noreply.github.com>
2021-04-30 14:09:08 +0800

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://user:user@store.kx3iq.mongodb.net/StoreAPI?retryWrites=true&w=majority";
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/StoreAPI', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-mongoose.connect(uri || 'mongodb://localhost:27017/StoreAPI', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/StoreAPI', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 module.exports = { mongoose }
